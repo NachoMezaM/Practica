4efabf3993af5f7c80244d11aa9268e4d88fdef1
NachoMezaM <nachom1605@gmail.com>
2024-09-04 10:27:34 -0400

    Hola

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { IndexComponent } from './post/index/index.component';
+import { ViewComponent } from './post/view/view.component';
+import { DeleteComponent } from './post/delete/delete.component';
+import { EditComponent } from './post/edit/edit.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'post/index', pathMatch: 'full'},
+    { path: 'post/index', component: IndexComponent },
+    { path: 'post/:postId/view', component: ViewComponent },
+    { path: 'post/delete', component: DeleteComponent },
+    { path: 'post/:postId/edit', component: EditComponent } 
+];
